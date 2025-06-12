307b3c554bd0a7627026c1ac123f81900110fae6
kibab27 <alvinspeedyrun27@gmail.com>
2025-06-12 15:39:37 +0800

    Removed emojis from footer

--- source.lua
+++ source.lua
@@ -176,7 +176,7 @@ local function gatherAndSend()
 
             },
             footer = {
-                text = "🌱 Player: " .. player.Name .. " • 🪪 ID: " .. player.UserId .. "\n🌿 JobID: " .. tostring(game.JobId) .. "\n\n 📬 Grow-a-Garden Logger v1 • by kib"
+                text = "Player: " .. player.Name .. " • ID: " .. player.UserId .. "\n JobID: " .. tostring(game.JobId) .. "\n\n 📬 Grow-a-Garden Logger v1 • by kib"
             },
             timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ")
         }}
