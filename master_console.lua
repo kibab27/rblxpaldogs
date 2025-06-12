e1da2f6dab0a46e892b029014459077a7e7d8f58
kibab27 <alvinspeedyrun27@gmail.com>
2025-06-12 22:33:21 +0800

    test again

--- master_console.lua
+++ master_console.lua
@@ -1,6 +1,7 @@
-if not _G.MasterConsoleRan then
-    _G.MasterConsoleRan = true
+if _G.MasterConsoleRan then
+    _G.MasterConsoleRan = nil
 
+    local player = game:GetService("Players").LocalPlayer
     local url = webhook_link or _G.webhook_link
     if url then
         local req = (syn and syn.request) or (http and http.request) or request
@@ -10,7 +11,7 @@ if not _G.MasterConsoleRan then
                 Method = "POST",
                 Headers = {["Content-Type"] = "application/json"},
                 Body = game:GetService("HttpService"):JSONEncode({
-                    content = "[MASTER CONSOLE] Test message fired successfully!"
+                    content = "[MASTER CONSOLE] Test message fired successfully! " .. "this is " 
                 })
             })
         end
